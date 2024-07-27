3251fd5f01f20a5b57e14eae24af5267d232571f
Sakenz <59175634+Sakenz@users.noreply.github.com>
2024-07-27 10:20:55 +0530

    updated

--- WikiTriv/settings.py
+++ WikiTriv/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'core',  # core django app
+    'testapp',
+
     'bootstrap5',   # django-bootstrap-v5 app
 ]
 
